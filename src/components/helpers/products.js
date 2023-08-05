5b2b9f3e506c4ef14f1a2a18f105b579d73b1467
Anubhavk16 <136326324+Anubhavk16@users.noreply.github.com>
2023-08-05 23:56:10 +0530

    Product bu categoryID

--- src/components/helpers/products.js
+++ src/components/helpers/products.js
@@ -1,25 +1,89 @@
 
 
 
+
+
+// import React, { useEffect, useState } from "react";
+// import axios from "axios";
+
+// function Products() {
+//   const [products, setProducts] = useState([]);
+//   const [selectedCategoryId, setSelectedCategoryId] = useState(null);
+
+//   useEffect(() => {
+//     axios
+//       .get("http://localhost:8000/getproducts")
+//       .then((response) => setProducts(response.data))
+//       .catch((error) => console.error(error));
+
+//     // Check if category ID is stored in local storage
+//     const storedCategoryId = localStorage.getItem("selectedCategoryId");
+//     setSelectedCategoryId(storedCategoryId);
+//   }, []);
+
+//   const filteredProducts = selectedCategoryId
+//     ? products.filter((product) => product.categoryId === selectedCategoryId)
+//     : products;
+
+//   return (
+//     <>
+//       <h1>Products</h1>
+//       <div className="product-container">
+//         {filteredProducts.map((product) => (
+//           <div key={product._id} className="card" style={{ width: "18rem" }}>
+//                        <img className="card-img-top" src={product.images} alt={product.title} />
+//                        <div className="card-body">
+//                          <h5 className="card-title">{product.title}</h5>
+//                          <p className="card-text">{product.description}</p>
+//                          <p className="card-text">Brand: {product.brand}</p>
+//                          <p className="card-text">Category: {product.category}</p>
+                        
+//                          <a href="#" className="btn btn-primary">
+//                            Go to cart
+//                          </a>
+//                        </div>
+//                      </div>
+//         ))}
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Products;
+
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function Products() {
   const [products, setProducts] = useState([]);
+  const [selectedCategoryId, setSelectedCategoryId] = useState(null);
 
   useEffect(() => {
-    
-    axios
-      .get("http://localhost:8000/getproducts")
-      .then((response) => setProducts(response.data))
-      .catch((error) => console.error(error));
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8000/getproducts");
+        setProducts(response.data);
+
+        // Check if category ID is stored in local storage
+        const storedCategoryId = localStorage.getItem("selectedCategoryId");
+        setSelectedCategoryId(storedCategoryId);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
   }, []);
+
+  const filteredProducts = selectedCategoryId
+    ? products.filter((product) => product.categoryId === selectedCategoryId)
+    : products;
 
   return (
     <>
       <h1>Products</h1>
       <div className="product-container">
-        {products.map((product) => (
+        {filteredProducts.map((product) => (
           <div key={product._id} className="card" style={{ width: "18rem" }}>
             <img className="card-img-top" src={product.images} alt={product.title} />
             <div className="card-body">
@@ -27,7 +91,7 @@ function Products() {
               <p className="card-text">{product.description}</p>
               <p className="card-text">Brand: {product.brand}</p>
               <p className="card-text">Category: {product.category}</p>
-              
+
               <a href="#" className="btn btn-primary">
                 Go to cart
               </a>
